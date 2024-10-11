8ff9d09b73ca1ae83deebbf5e984759dfadbfe73
greengoblin77 <jgonzalez@fwdcostarica.com>
2024-10-10 18:10:23 -0600

    terminado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -68,9 +68,34 @@ const deleteProduct = async (req, res) => {
     }
 };
 
+const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const { nombre, cantidad, price, description } = req.body;
+
+    try {
+        const data = await fs.readFile('datos.json', 'utf8');
+        let productos = JSON.parse(data);
+
+        const index = productos.findIndex(prod => prod.id === id);
+        if (index === -1) {
+            return res.status(404).json({ message: 'Producto no encontrado' });
+        }
+
+        if (nombre !== undefined) productos[index].nombre = nombre;
+        if (cantidad !== undefined) productos[index].cantidad = cantidad;
+        if (price !== undefined) productos[index].price = price;
+        if (description !== undefined) productos[index].description = description;
+
+        await fs.writeFile('datos.json', JSON.stringify(productos, null, 2)); 
+        res.status(200).json(productos[index]); 
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar el producto', error: error.message });
+    }
+};
 
 module.exports = {
     getAllproductos,
     createProduct,
-    deleteProduct
-}
+    deleteProduct,
+    updateProduct 
+};
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,5 +5,6 @@ const productsController = require('../controllers/productsController');
 router.get('/', productsController.getAllproductos);
 router.post('/', productsController.createProduct);
 router.delete('/:id', productsController.deleteProduct); 
+router.put('/:id', productsController.updateProduct);
 
 module.exports = router;
